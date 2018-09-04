c85397ebfb865bd400cfbec0955a76a195290830
RTongue <r.tongue@gmail.com>
2018-09-03 21:21:10 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-var, vars-on-top */
+
+function parseIFrame(iframeStr) {
+  if (iframeStr.length < 1 || !iframeStr.includes('src="')) {
+    alert('Enter a valid iframe element');
+    return '';
+  }
+  const splitStr = iframeStr.split('src="')[1];
+  const endIdx = splitStr.indexOf('"');
+  return splitStr.slice(0, endIdx);
+}
+
+window.onload = function() {
+  var mp3 = document.getElementById('mashup');
+  var playButton = document.getElementsByClassName('btn-container')[0];
+  var slideSrc = document.getElementById('slideSrc');
+
+  playButton.addEventListener('click', () => {
+    var src = parseIFrame(slideSrc.value);
+    if (src.length === 0) return;
+    playButton.classList.add('hidden');
+    slideSrc.classList.add('hidden');
+
+    var iframeDom = document.createElement('iframe');
+    iframeDom.id = 'slideshow';
+    iframeDom.src = src;
+    iframeDom.frameborder = '0';
+    iframeDom.width = window.innerWidth;
+    iframeDom.height = window.innerHeight;
+    iframeDom.setAttribute('allowFullScreen', 'true');
+    iframeDom.setAttribute('mozallowfullscreen', 'true');
+    iframeDom.setAttribute('webkitallowfullscreen', 'true');
+    iframeDom.onload = function onIframeLoad() {
+      mp3.play();
+    };
+
+    document.body.appendChild(iframeDom);
+  });
+};
